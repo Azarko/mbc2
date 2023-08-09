12044ef25b9d3092fcff96d276f60b7d85a137b3
Azarko <mr.azarko@gmail.com>
2023-08-09 22:21:52 +0300

    add jinja2 skeleton

--- mbc2/main.py
+++ mbc2/main.py
@@ -1,10 +1,21 @@
+import logging
+import os.path
+
 from aiohttp import web
+import aiohttp_jinja2
+import jinja2
 
 from mbc2 import routes
 
+BASE_PATH = os.path.dirname(__file__)
+TEMPLATES_DIR = os.path.join(BASE_PATH, 'templates')
+TEMPLATES_LOADER = jinja2.FileSystemLoader(TEMPLATES_DIR)
+
 
 def create_app() -> web.Application:
+    logging.basicConfig(level=logging.INFO)
     app = web.Application()
+    aiohttp_jinja2.setup(app, enable_async=True, loader=TEMPLATES_LOADER)
     routes.setup_routes(app)
     return app
 
--- tests/handlers/index.py
+++ tests/handlers/index.py
@@ -5,4 +5,4 @@ async def test_index(web_app_client):
     response = await web_app_client.get('/')
     assert response.status == http.HTTPStatus.OK
     body = await response.text()
-    assert body == 'Hello, world!'
+    assert 'Hello, world!' in body
