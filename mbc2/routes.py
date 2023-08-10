6b98a29f725bb9bf6a26bf73cc3a7370d292fc99
Azarko <mr.azarko@gmail.com>
2023-08-10 09:15:39 +0300

    add some logic

--- mbc2/routes.py
+++ mbc2/routes.py
@@ -1,5 +1,14 @@
-from mbc2.handlers import index
+from mbc2.handlers.web import index
+from mbc2.handlers.web import party_calc
+from mbc2.handlers.ajax import party_calc_calculate
 
 
 def setup_routes(app):
     app.router.add_get('/', index.handler, name='index')
+    app.router.add_get('/party-calc', party_calc.handler, name='party_calc')
+
+    app.router.add_post(
+        '/party-calc/calculate',
+        party_calc_calculate.handler,
+        name='party_calc_calculate',
+    )
