d3baeafcf652bf1710f04bb2c1568497159625a7
Azarko <mr.azarko@gmail.com>
2023-08-23 00:47:10 +0300

    add dynamic generation and deletion of calculator form rows

--- mbc2/routes.py
+++ mbc2/routes.py
@@ -1,6 +1,11 @@
+import os.path
+
 from mbc2.handlers.ajax import calculate
 from mbc2.handlers.web import index
 from mbc2.handlers.web import party_calc
+
+
+CURRENT_FOLDER = os.path.dirname(__file__)
 
 
 def setup_routes(app):
@@ -11,4 +16,14 @@ def setup_routes(app):
         '/party-calc/calculate',
         calculate.handler,
         name='party_calc_calculate',
+    )
+    # probably, it shouldn't be called on production
+    setup_static_routes(app)
+
+
+def setup_static_routes(app):
+    app.router.add_static(
+        '/static/',
+        path=os.path.join(CURRENT_FOLDER, 'static'),
+        name='static',
     )
