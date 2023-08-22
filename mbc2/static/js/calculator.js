30ebb9e50c340c8de1428f665fe640748ec08eef
Azarko <mr.azarko@gmail.com>
2023-08-23 01:39:11 +0300

    send valid data to backend

--- mbc2/static/js/calculator.js
+++ mbc2/static/js/calculator.js
@@ -5,14 +5,14 @@ var member_index = 1;
 // add new row in calculator form and increment member_index
 function add_member() {
   var $row = $(`
-    <div class="row" id="member-${member_index}">
+    <div class="row" id="member-row-${member_index}">
       <div class="col-auto">
         <label for="member-name-${member_index}" class="visually-hidden">Name</label>
         <input type="text" class="form-control" id="member-name-${member_index}" placeholder="Name" value="member_${member_index}">
       </div>
       <div class="col-auto">
         <label for="member-paid-${member_index}" class="visually-hidden">Paid</label>
-        <input type="text" class="form-control" id="member-paid-${member_index}" placeholder="Paid">
+        <input type="text" class="form-control" id="member-paid-${member_index}" placeholder="Paid" value=0.0>
       </div>
       <div class="col-auto">
         <button type="button" class="btn btn-danger mb-3" id="delete-member-${member_index}">Del</button>
@@ -21,6 +21,18 @@ function add_member() {
   `);
   $('#calculate-form').append($row);
   member_index += 1;
+}
+
+function get_form_data() {
+  var data_array = [];
+
+  $('div[id^=member-row-]').each(function () {
+    var name = $(this).find("input[id^='member-name-']").val();
+    var paid = $(this).find("input[id^='member-paid-']").val();
+    data_array.push({'name': name, 'paid': paid});
+  });
+
+  return data_array;
 }
 
 add_member();  // add first initial row in the form
@@ -44,18 +56,14 @@ $("body").on('click', "button[id^='delete-member-']", function() {
 
 // submit form (Calculate button)
 $("#calculate-form").submit(function (event) {
+  var data_array = get_form_data();
+
   $.ajax({
-    type: "POST",
-    url: "/party-calc/calculate",
+    type: 'POST',
+    url: '/party-calc/calculate',
     async: false,
-    data: JSON.stringify(
-      { members: [
-        {name: "Vasya", paid: 100},
-        {name: "Petya", paid: 50},
-        ],
-      },
-    ),
-    contentType: "application/json",
+    data: JSON.stringify({ 'members': data_array }),
+    contentType: 'application/json',
   }).done(function (data) {
     console.log(data);
   });
